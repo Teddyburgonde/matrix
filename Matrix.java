8edc2372e2073a519cb96ff8b9ab597b677f3349
Teddyburgonde <bandama.teddy@gmail.com>
2025-08-16 23:44:55 +0200

    added file Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -1,25 +1,4 @@
-		// Crée une matrice 2×2 de type double vide (remplie de 0.0).
-
-		//	Mets la valeur 5.0 dans la première ligne, deuxième colonne.
-
-		//	Mets la valeur -3.0 dans la deuxième ligne, première colonne.
-
-		//	Affiche ces deux valeurs avec System.out.println(...).
-
-public class Matrix { 
-
-	public static void printMatrix(double matrix[][])
-    	{
-		for (int i = 0; i < matrix.length; i++)
-		{
-			for (int j = 0; j < matrix[i].length; j++)
-			{
-				System.out.print( matrix[i][j]  + "\t");
-	     		}
-	     		System.out.println();
-	   	}
-	}
-
+/*
 	public static void main(String[] args) {
 			
 		// 1. Crée une matrice double[3][3].
@@ -43,4 +22,27 @@ public class Matrix {
 		 
 	} 
 
+*/
+
+
+public class Matrix { 
+
+	public static void printMatrix(double matrix[][])
+    	{
+		for (int i = 0; i < matrix.length; i++)
+		{
+			for (int j = 0; j < matrix[i].length; j++)
+			{
+				System.out.print( matrix[i][j]  + "\t");
+	     		}
+	     		System.out.println();
+	   	}
+	}
+	
+	public static void main(String[] args) { 
+
+		
+
+	}
 }
+	
