274c38abad9bdb644a8c0ecbe44bbc4fdb00a7c5
Teddy Bandama <tebandam@1G7.42angouleme.fr>
2025-08-17 06:37:59 +0200

    updated readme.md

--- Matrix.java
+++ Matrix.java
@@ -1,4 +1,18 @@
 /*
+
+	public static void printMatrix(double matrix[][])
+    	{
+		for (int i = 0; i < matrix.length; i++)
+		{
+			for (int j = 0; j < matrix[i].length; j++)
+			{
+				System.out.print( matrix[i][j]  + "\t");
+	     		}
+	     		System.out.println();
+	   	}
+	}
+
+
 	public static void main(String[] args) {
 			
 		// 1. Crée une matrice double[3][3].
@@ -27,41 +41,33 @@
 
 Crée deux vecteurs double[] de dimension 3 :
 
-v1=(2,4,−1)
-v1=(2,4,−1)
+v1=(10,−2,5)
+v2=(3,7,−1)
 
-v2=(5,0,3)
-v2=(5,0,3)
-
-Crée un troisième vecteur result qui sera la somme des deux (formule :
+Crée un troisième vecteur result qui sera la somme des deux 
 
 result[i]=v1[i]+v2[i]
-result[i]=v1[i]+v2[i] ).
 
 Affiche le résultat sous la forme :
-
+[7.0, -9.0, 6.0]
 */
 
 
+import java.util.Arrays;
 
 public class Matrix { 
 
-	public static void printMatrix(double matrix[][])
-    	{
-		for (int i = 0; i < matrix.length; i++)
-		{
-			for (int j = 0; j < matrix[i].length; j++)
-			{
-				System.out.print( matrix[i][j]  + "\t");
-	     		}
-	     		System.out.println();
-	   	}
-	}
-	
-	public static void main(String[] args) { 
-
+	public static void main(String[] args) 
+	{ 
+		double[] v1 = {10.0, -2.0, 5.0};
+		double[] v2 = {3.0, 7.0, -1.0};
 		
-
+		double[] sums = new double[v1.length];
+		
+		for (int i = 0; i < sums.length; ++i) {
+			sums[i] = v1[i] - v2[i];
+		}
+		System.out.println(Arrays.toString(sums));
 	}
 }
 	
--- README.md
+++ README.md
@@ -3,15 +3,14 @@
 ## 🟢 VECTEURS
 - ✅ Créer un vecteur en Java (`double[]`)
 - ✅ Lire/écrire une valeur dans un vecteur (`v[0]`, `v[1]` …)
-- ❌ Addition de deux vecteurs (`v1 + v2`)
-- ❌ Soustraction de deux vecteurs (`v1 - v2`)
+- ✅ Addition de deux vecteurs (`v1 + v2`)
+- ✅ Soustraction de deux vecteurs (`v1 - v2`)
 - ❌ Multiplication par un scalaire (`c * v`)
-- ❌ Calculer la norme d’un vecteur (longueur)
 
 ## 🟢 MATRICES
 - ✅ Créer une matrice (`double[][]`)
 - ✅ Lire/écrire une valeur dans une matrice (`matrix[i][j]`)
-- ❌ Addition de deux matrices
+- ✅ Addition de deux matrices
 - ❌ Soustraction de deux matrices
 - ❌ Multiplication par un scalaire (`c * M`)
 - ❌ Multiplication matrice × vecteur
@@ -19,5 +18,5 @@
 ## 🟢 PROGRAMMATION JAVA
 - ✅ Être à l’aise avec les indices (commencent à 0)
 - ✅ Savoir afficher une matrice avec `printMatrix`
-- ❌ Utiliser une boucle `for` pour afficher un vecteur
-- ❌ Utiliser des boucles `for` imbriquées pour manipuler une matrice
+- ✅ Utiliser une boucle `for` pour afficher un vecteur
+- ✅ Utiliser des boucles `for` imbriquées pour manipuler une matrice
