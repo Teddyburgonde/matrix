4e12da07201761777805b7f81daf5ddf47eff0fd
Teddy Bandama <tebandam@1F6.42angouleme.fr>
2025-08-18 11:29:15 +0200

    finished ex01

---
+++ ex01/Matrix.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Matrix {
+
+	public static double[] linearCombination(double[][] vectors, double[] coeffs)
+	{
+		double[] result = new double[vectors[0].length];
+		
+		for (int i = 0; i < vectors[0].length; ++i) { 
+			for (int j = 0; j < vectors.length; ++j) { 
+				result[i] += coeffs[j] * vectors[j][i]; 
+			} 
+		}
+		
+		return result;
+	}
+}
