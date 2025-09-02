abae0089cc8356db8d05e00b49d8310d0c3b9e75
Teddyburgonde <bandama.teddy@gmail.com>
2025-09-02 21:08:46 +0200

    finished ex09

---
+++ ex09/Transpose.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Transpose {
+
+	public static double[][] transpose(double[][] A) {
+		double[][] T = new double[A[0].length][A.length];
+		
+		for (int i = 0; i < A.length; i++) {
+			for (int j = 0; j < A[0].length; ++j) {
+				T[j][i] = A[i][j];
+			}
+			
+		}
+		return T;
+	}
+
+	public static void main(String[] args) {
+		
+		double[][] A = {
+			{1, 2, 3},
+      			{4, 5, 6}
+		};
+		
+		System.out.println(Arrays.deepToString(transpose(A)));
+		// [[1.0, 4.0], [2.0, 5.0], [3.0, 6.0]]
+	}
+
+}
