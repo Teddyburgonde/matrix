e0c9daefc2229f04cf9307876e46800927324acc
Teddyburgonde <bandama.teddy@gmail.com>
2025-09-04 21:24:18 +0200

    finished ex05

---
+++ ex05/Cos.java
@@ -0,0 +1,36 @@
+public class Cos {
+
+	public static double norm2(double[] v) { 
+	
+	double R = 0; 
+		for (int i = 0; i < v.length; ++i) {
+			R += Math.pow(v[i], 2); 
+		} 
+		return Math.sqrt(R); 
+	}
+
+	public static double cosine(double[] u, double[] v) {
+		double resultScalaire = 0;
+		if ( u.length != v.length)
+			throw new IllegalArgumentException("Incorrect vector values");
+		for (int i = 0; i < u.length; ++i) {			
+			resultScalaire += u[i] * v[i];
+		}
+		double resultFinal = resultScalaire / (norm2(u) * norm2(v));
+		return resultFinal; 
+	}
+
+	public static void main(String[] args) {
+ 		double[] u1 = {1, 0};
+		double[] v1 = {0, 1};
+		System.out.println(cosine(u1, v1)); // attendu : 0.0
+
+		double[] u2 = {1, 0};
+		double[] v2 = {1, 0};
+		System.out.println(cosine(u2, v2)); // attendu : 1.0
+
+		double[] u3 = {1, 0};
+		double[] v3 = {-1, 0};
+		System.out.println(cosine(u3, v3)); // attendu : -1.0
+	}
+}
