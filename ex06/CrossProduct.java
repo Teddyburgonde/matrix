b9004a72223aec0097ebff0d06fdcb01c94234f8
Teddy Bandama <tebandam@1G6.42angouleme.fr>
2025-09-06 11:36:55 +0200

    finished 06

--- ex06/CrossProduct.java
+++ ex06/CrossProduct.java
@@ -1,43 +1,37 @@
-/*
+public class CrossProduct {
 
-u = (2, 3, 4)
-v = (1, 0, -1)
+	public static double[] crossProduct(double[] u, double[] v) {
+		double x = 0;
+		x = u[1] * v[2] - u[2] * v[1];
+		
+		double y = 0;
+		y = u[2] * v[0] - u[0] * v[2];
+		
+		double z = 0;
+		z = u[0] * v[1] - u[1] * v[0];
+		
+		double[] result = new double[3];
+		result[0] = x;
+		result[1] = y;
+		result[2] = z;
 
-pour x 
-la formule : (u_y * v_z) - (u_z * v_y)
-u_y = 3 u_z = 4
-v_y = 0 v_z = -1
+		return result;
+	}
+	
+	public static void main(String[] args) {
+    	double[] u1 = {0.0, 0.0, 1.0};
+    	double[] v1 = {1.0, 0.0, 0.0};
+    	System.out.println(java.util.Arrays.toString(crossProduct(u1, v1)));
+    	// expected : [0.0, 1.0, 0.0]
 
-donc 3 * -1 = -3 
-4 * 0 = 0
-donc -3 - 0 = -3
+    	double[] u2 = {1.0, 2.0, 3.0};
+    	double[] v2 = {4.0, 5.0, 6.0};
+    	System.out.println(java.util.Arrays.toString(crossProduct(u2, v2)));
+    	// expected: [-3.0, 6.0, -3.0]
 
-donc x = -3 
-
-
-pour y 
-la formule : (u_z * v_x) - (u_x * v_z)
-
-u_z = 3eme coordonner de u
-v_x = 1ere coordonner de v
-u_x = 1ere coordonner de u
-v_z = 3eme coordonner de v 
-
-donc (4 * 1) - (2 * -1) 
-4 - (-2)
-y = 6 
-
-donc (-3, 6, ?)
-
-
-pour z 
-la formule (u_x * v_y) - (u_y * v_x)
-(2 * 0 ) - ( 3 * 1) 
-0 - 3
-z = -3
-
-donc (-3, 6, -3)
-
-*/
-
-public static double[] crossProduct(double[] u, double[] v)
+    	double[] u3 = {4.0, 2.0, -3.0};
+    	double[] v3 = {-2.0, -5.0, 16.0};
+    	System.out.println(java.util.Arrays.toString(crossProduct(u3, v3)));
+    	// expected : [17.0, -58.0, -16.0]
+	}
+}
