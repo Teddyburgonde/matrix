6140f236c091fa417fd01bca85731861d62ada74
Teddy Bandama <tebandam@1G7.42angouleme.fr>
2025-09-07 19:28:00 +0200

    I started ex11

---
+++ ex11/Inverse.java
@@ -0,0 +1,30 @@
+A= [1 2 3]                [a b c] 
+    [0 1 4] 			  [d e f] 
+	[5 6 0]               [g h i]
+
+
+
+det(A)=a(ei−fh)−b(di−fg)+c(dh−eg)
+
+(1 * 0) - (4 * 6) = 0 - 24 = -24 
+
+(0 * 0) - (4 * 5) = 0 - 20 = -20
+
+(0 * 6) - ( 1 * 5 ) = 0 - 5 = -5 
+
+
+1 * -24 = -24 
+2 * - 20 = + 40 
+
+3 * - 5 = -15
+
+(-24) - (-40) + (-15) =   1
+
+
+public class Determinant {
+
+	public static double determinant2x2(double[][] A) {
+			
+	
+	}
+}
